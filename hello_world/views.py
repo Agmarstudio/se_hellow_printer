fe2cc4f07f52ee9927e92d8952508448a11e759d
agmarstudio <marcin.keblesz@gmail.com>
2020-04-26 06:52:50 +0000

    add automatic deploy by travis

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,7 @@ from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
 msg = "Hello World!"
-moje_imie = "Marcin TEST HEROKU"
+moje_imie = "Marcin TEST AUTOMATIC DEPLOY"
 
 
 @app.route('/')
